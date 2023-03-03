ce42bbc4d9e3c2654165bf09fa6a6459da04872c
Chinmay <chinmaymp007@gmail.com>
2023-03-03 23:02:51 +0530

    Did some new changes

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,32 @@
 'use strict';
+
+const model = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnclosemodel = document.querySelector('.close-modal');
+const btnOpenModel = document.querySelectorAll('.show-modal');
+console.log(btnOpenModel);
+
+for (let i = 0; i < btnOpenModel.length; i++) {
+    btnOpenModel[i].addEventListener('click', function () {
+        console.log("Button clicked");
+        model.classList.remove('hidden');
+        overlay.classList.remove('hidden');
+    });
+}
+
+const closeModel = function () {
+    model.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+btnclosemodel.addEventListener('click', function () {
+    closeModel();
+});
+
+overlay.addEventListener('click', closeModel);
+
+document.addEventListener('keydown', function (e) {
+    if (!model.classList.contains('hidden') && e.key == 'Escape') {
+        closeModel();
+    }
+});
